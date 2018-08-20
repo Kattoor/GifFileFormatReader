3f4ee0f7aa055eb3b870be53fb7bf68a93e9c9cf
Kattoor <jasper.catthoor@gmail.com>
2018-08-20 23:15:46 +0200

    stuff

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
@@ -20,7 +17,7 @@ public class Main {
     private int amountOfColorTableEntries;
 
     private Main() throws IOException {
-        File file = new File("yugioh.gif");
+        File file = new File("hamburger.gif");
         data = Files.readAllBytes(file.toPath());
         pointer = 6;
 
@@ -33,8 +30,6 @@ public class Main {
             int nextByte = readByte();
             switch (nextByte) {
                 case 0x2C:
-                    /*System.out.println(pointer);
-                    System.out.println(Integer.toHexString(pointer));*/
                     readBody();
                     break;
                 case 0x21:
@@ -53,17 +48,17 @@ public class Main {
     private void readHeaderInHeader() {
         int screenWidth = readWord();
         int screenHeight = readWord();
-        packed = readByte();
-        int backgroundColor = readByte();
+        packed = readByte(); // 1100 0100
+        int backgroundColor = readByte(); // 15 => 21
         int aspectRatio = readByte();
     }
 
     private void readLogicalScreenDescriptionInHeader() {
         int globalColorTableSize = packed & 0b111;
         int colorTableSortFlag = (packed & 0b1000) >> 3;
-        int colorResolution = (packed & 0b1110000) >> 4;
+        int colorResolution = (packed & 0b1110000) >> 4; // 4
         int globalColorTableFlag = (packed & 0b10000000) >> 7;
-        amountOfColorTableEntries = 1 << (globalColorTableSize + 1);
+        amountOfColorTableEntries = 1 << (globalColorTableSize + 1); // 32
     }
 
     private int[][] colorTable;
@@ -78,19 +73,14 @@ public class Main {
         }
 
         for (int[] i : colorTable) {
-            int rgb = i[0]; //
+            int rgb = i[0];
             rgb = (rgb << 8) + i[1];
             rgb = (rgb << 8) + i[2];
-            List<Integer> l = new ArrayList<>();
-            l.add(rgb);
-            dictionary.add(l);
+            dictionary.add(Collections.singletonList(rgb));
         }
-        List<Integer> clearCode = new ArrayList<>();
-        clearCode.add(-1);
-        dictionary.add(clearCode);
-        List<Integer> endOfInformationCode = new ArrayList<>();
-        endOfInformationCode.add(-2);
-        dictionary.add(endOfInformationCode);
+
+        dictionary.add(Collections.singletonList(-1));
+        dictionary.add(Collections.singletonList(-2));
     }
 
     private void readExtensionInformation() {
@@ -106,11 +96,11 @@ public class Main {
     }
 
     private void readApplicationExtensionBlock() {
-        int blockSize = readByte();     // 0Bh for  for applicationextensionblock
+        int blockSize = readByte(); // 0B for applicationextensionblock
         int[] identifier = {readByte(), readByte(), readByte(), readByte(), readByte(), readByte(), readByte(), readByte()};
         int[] authentCode = {readByte(), readByte(), readByte()};
 
-        int applicationDataSize = readByte(); // size of coming sub blocks in bytes
+        int applicationDataSize = readByte(); // size of coming sub blocks in bytes 3C
         boolean lastSubBlockReached = applicationDataSize == 0;
         int subBlockDataCount = 0;
         while (!lastSubBlockReached) {
@@ -201,16 +191,6 @@ public class Main {
         BufferedImage image = new BufferedImage(500, 375, BufferedImage.TYPE_INT_RGB);
         for (int y = 0; y < 375; y++) {
             for (int x = 0; x < 500; x++) {
-                /*value = red;
-        value = (value << 8) + green;
-        value = (value << 8) + blue;*/
-                /*int[] ints = new int[]{0, 0, 0};
-                if (y * 375 + x < total.size() && total.get(y * 375 + x) >= 0)
-                    ints = colorTable[total.get(y * 375 + x)];
-                int rgb = ints[0];
-                rgb = (rgb << 8) + ints[1];
-                rgb = (rgb << 8) + ints[2];*/
-                //System.out.println(y + " " + x) ;
                 if (y * 500 + x < decompressed.size()) {
                     int rgb = decompressed.get(y * 500 + x);
                     image.setRGB(x, y, rgb);
@@ -287,15 +267,15 @@ public class Main {
                     int third = bytes.get((bitOffset / 8) + 2);
                     int mod = bitOffset % 8;
                     if (mod == 0)
-                        code = ((first & 0b11111111) | ((second & 0b00000011) << 8)); //
+                        code = ((first & 0b11111111) | ((second & 0b00000011) << 8));
                     else if (mod == 1)
                         code = (((first & 0b11111110) >> 1) | ((second & 0b00000111) << 7));
                     else if (mod == 2)
-                        code = (((first & 0b11111100) >> 2) | ((second & 0b00001111) << 6)); //
+                        code = (((first & 0b11111100) >> 2) | ((second & 0b00001111) << 6));
                     else if (mod == 3)
                         code = (((first & 0b11111000) >> 3) | ((second & 0b00011111) << 5));
                     else if (mod == 4)
-                        code = (((first & 0b11110000) >> 4) | ((second & 0b00111111) << 4)); //
+                        code = (((first & 0b11110000) >> 4) | ((second & 0b00111111) << 4));
                     else if (mod == 5)
                         code = (((first & 0b11100000) >> 5) | ((second & 0b01111111) << 3));
                     else if (mod == 6)
@@ -310,15 +290,15 @@ public class Main {
                     int third = bytes.get((bitOffset / 8) + 2);
                     int mod = bitOffset % 8;
                     if (mod == 0)
-                        code = ((first & 0b11111111) | ((second & 0b00000111) << 8)); //
+                        code = ((first & 0b11111111) | ((second & 0b00000111) << 8));
                     else if (mod == 1)
                         code = (((first & 0b11111110) >> 1) | ((second & 0b00001111) << 7));
                     else if (mod == 2)
-                        code = (((first & 0b11111100) >> 2) | ((second & 0b00011111) << 6)); //
+                        code = (((first & 0b11111100) >> 2) | ((second & 0b00011111) << 6));
                     else if (mod == 3)
                         code = (((first & 0b11111000) >> 3) | ((second & 0b00111111) << 5));
                     else if (mod == 4)
-                        code = (((first & 0b11110000) >> 4) | ((second & 0b01111111) << 4)); //
+                        code = (((first & 0b11110000) >> 4) | ((second & 0b01111111) << 4));
                     else if (mod == 5)
                         code = (((first & 0b11100000) >> 5) | ((second & 0b11111111) << 3));
                     else if (mod == 6)
@@ -360,27 +340,14 @@ public class Main {
 
                         int c = bytes.get((bitOffset) / 8);
                         usedCodes.add(c);
-                        //currentCodeValue = dictionary.get(c);
-                        //indices.add(currentCodeValue.get(0));
+                        currentCodeValue = dictionary.get(c);
+                        indices.add(currentCodeValue.get(0));
                         previousCodeValue = new ArrayList<>();
                         previousCodeValue.addAll(dictionary.get(c));
-
-                        // currentCodeValue = new ArrayList<>();
-                        //currentCodeValue.add((int) Math.pow(2, 8) - 1);
-                        // previousCodeValue = new ArrayList<>();
-
-                        //  currentCodeValue = new ArrayList<>();
-                        // previousCodeValue = new ArrayList<>();
-                        //   resetted = 0;
                     }
                 }
 
                 bitOffset += bitsToRead;
-
-                if (resetted > -1)
-                    resetted++;
-
-                System.out.println(resetted);
 
                 usedCodes.add(code);
 
@@ -389,20 +356,14 @@ public class Main {
                     if (code != 128) {
 
                         if (dictionary.size() > code) {
-                            if (indices.size() > 22990)
-                                System.out.println("ok");
                             currentCodeValue = dictionary.get(code);
                             indices.addAll(new ArrayList<>(currentCodeValue));
                             int k = currentCodeValue.get(0);
                             List<Integer> toAdd = new ArrayList<>(previousCodeValue);
                             toAdd.add(k);
                             dictionary.add(toAdd);
-                            if (dictionary.size() == Math.pow(2, bitsToRead) && bitsToRead < 12) {
-                                // if (code == 75)
-                                //      return indices;
-                                // else
+                            if (dictionary.size() == Math.pow(2, bitsToRead) && bitsToRead < 12)
                                 bitsToRead++;
-                            }
                             previousCodeValue = new ArrayList<>(currentCodeValue);
                         } else {
                             int k = previousCodeValue.get(0);
